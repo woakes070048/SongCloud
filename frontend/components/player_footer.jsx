ef31f624e6d6606918b73c5a3239281488be58cf
EricMoy <ericmoy1989@gmail.com>
2016-06-29 10:23:47 -0400

    initial styling for footer player

--- frontend/components/player_footer.jsx
+++ frontend/components/player_footer.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var PlayerFooterStore = require('../stores/player_footer_store');
 var PlayerFooterActions = require('../actions/player_footer_actions');
+var Link = require('react-router').Link;
 
 module.exports = React.createClass({
 
@@ -32,13 +33,40 @@ module.exports = React.createClass({
   },
 
   render: function () {
+    var song = this.state.song;
     var playButtonState = 'player-button-' + !this.state.playState;
-    var hiddenPlayer = this.state.song ? "" : "hidden-player";
+    var hiddenPlayer = "hidden-player";
+    var songInfo;
+    var songLink;
+    var divStyle;
+    if (song && song.user_id) {
+      hiddenPlayer = "";
+      songLink = song.user_id + '/' + song.artist + '/' + song.id + '/' + song.title;
+      divStyle = {
+        backgroundImage: 'url(' + song.image_url + ')'
+      };
+
+      songInfo = (
+        <div className="song-info-div group">
+          <Link to={songLink}>
+            <div className="song-index-img footer-img" style={divStyle} />
+          </Link>
+          <div className="text-container">
+            <Link to={song.user_id + '/' + song.artist} className="footer-playing">{song.artist}</Link>
+            <Link to={songLink} className="footer-title">{song.title}</Link>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div className={"player-footer " + hiddenPlayer}>
-        <div className="w-container">
-          <div className={ "song-index-play interactive fake-player " }>
-            <div className={ playButtonState } onClick={this.toggleStore} />
+        <div className="w-container group">
+          <div className="skip-control control-prev" />
+          <div className={ "play-control " + playButtonState } onClick={this.toggleStore} />
+          <div className="skip-control control-next" />
+          <div className="current-song-info">
+            {songInfo}
           </div>
           <audio id="player-footer" preload="metadata" />
         </div>
